fca888d7a28ce66e1801c9d17c257ea02dae5d91
AnnChariya <112073442+AnnChariya@users.noreply.github.com>
2023-10-16 09:57:10 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<body bgcolor="#CC9900">
+    <table width="1238" height="530" border="0">
+        <tr>
+            <td width="23" rowspan="2">&nbsp;</td>
+            <td height="119" colspan="2" bgcolor="#006600">
+                <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
+                <?php include "Date.php"; ?>
+            </td>
+            <td width="43" rowspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+            <td width="1007" bgcolor="#FFFFFF">
+                <img src="images/map.gif">
+            </td>
+            <td width="190" bgcolor="#FFFF66"><ul>
+				<li><a href="home.php">หน้าแรก</li>
+				<li><a href="room.php">ห้องพัก&ราคา</li>
+				<li><a herf="guest.php">สมุดเยี่ยม</li>
+				<li><a href="contact.php">ติดต่อ</li>
+				</ul><p>
+				<?php include "login_form.php";?>
+			</td>
+        </tr>
+    </table>
+</body>
